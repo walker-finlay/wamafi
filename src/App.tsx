8dcc9404ea5ddd568ce33c68b414d60514f9ef13
walker-finlay <walker.finlay@gmail.com>
2025-02-20 02:14:31 -0500

    svg logo, dark mode css only

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,16 @@
-import * as React from 'react';
 import './App.css';
-import darkLogoUrl from './assets/wd_full_dark.png';
-import lightLogoUrl from './assets/wd_full_light.png';
-
-
+import darkLogoUrl from './assets/wd_full.svg';
+import SVG from 'react-inlinesvg';
 
 function App() {
-  const [darkMode, setDarkMode] = React.useState<boolean>(false);
-
-  function getPreferredColorScheme(): boolean {
-    if (window.matchMedia) {
-      return window.matchMedia('(prefers-color-scheme: dark)').matches
-    }
-    return true;
-  }
-
-  function updateColorScheme() {
-    setDarkMode(getPreferredColorScheme());
-  }
-
-  React.useEffect(() => {
-    if (window.matchMedia) {
-      const colorSchemeQuery = window.matchMedia('(prefers-color-scheme: dark)');
-      colorSchemeQuery.addEventListener('change', updateColorScheme);
-      updateColorScheme();
-    }
-
-    return () => {
-      window.matchMedia('(prefers-color-scheme: dark)').removeEventListener('change', updateColorScheme);
-    }
-  }, []);
-
   return (
     <div className='App'>
       <div className='content'>
-        <img src={darkMode ? darkLogoUrl : lightLogoUrl} width={200} alt='wamafi digital' />
+        <SVG
+          src={darkLogoUrl}
+          width={200}
+          title={'wamafi digital'}
+        />
         {/* <p>games &middot; data &middot; art</p> */}
         {/* <p>contact: <a href='mailto:wamafi@wamafi.com'>wamafi@wamafi.com</a></p> */}
       </div>
